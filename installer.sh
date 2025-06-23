b138f5f2cdead9e1f48037dc1ae1ad39d77060ff
Luzian Hahn <luzian.hahn@iis.fraunhofer.de>
2025-06-23 22:45:00 +0200

    feat: add backup sync cronjob

--- installer.sh
+++ installer.sh
@@ -23,3 +23,20 @@ if [ ! -f $HOME/bin/termux-file-editor ];then
 else
     echo "Found existing termux-file-editor. Too afraid to overwrite. Do so manually..."
 fi
+
+if command -v crontab >/dev/null 2>&1; then 
+    echo "crontab found. Attempting installation of different cron files..."
+    crontab -l > $SRC_DIR/tmp/cron.bak 2>/dev/null
+    for cronjob_bin in ./cron/*;do
+        echo "Trying cronfile: $cronjob_bin..."
+        cronjob_bin_file=$(basename $cronjob_bin)
+        # grep exits with 1 in case of 0 line matches. Especially problematic with empty crontab. Hence " || true"
+        cat $SRC_DIR/tmp/cron.bak | grep -v $cronjob_bin_file > $SRC_DIR/tmp/cleaned_cron.bak || true  
+        echo "0 5 * * * $ABS_SRC_DIR/cron/$cronjob_bin_file" >> $SRC_DIR/tmp/cleaned_cron.bak
+        mv $SRC_DIR/tmp/cleaned_cron.bak $SRC_DIR/tmp/cron.bak
+    done
+    crontab $SRC_DIR/tmp/cron.bak
+    rm $SRC_DIR/tmp/cron.bak
+else
+    echo "crontab not found. Hence skipping the installation of cronjobs..."
+fi

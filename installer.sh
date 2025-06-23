9c318121f2602f3c2c5550ae9619759e105a7663
Luzian Hahn <luzian.hahn@iis.fraunhofer.de>
2025-06-23 22:32:02 +0200

    refactor: export entrypoints into bin folder

--- installer.sh
+++ installer.sh
@@ -5,8 +5,8 @@ SRC_DIR=$(dirname "${BASH_SOURCE[0]}")
 ABS_SRC_DIR=$(realpath $SRC_DIR)
 
 mkdir -p $HOME/.local/bin
-ln -fs $ABS_SRC_DIR/compress_video_files $HOME/.local/bin/
-ln -fs $ABS_SRC_DIR/sync_local_dir_w_remote $HOME/.local/bin/
+ln -fs $ABS_SRC_DIR/bin/compress_video_files $HOME/.local/bin/
+ln -fs $ABS_SRC_DIR/bin/sync_local_dir_w_remote $HOME/.local/bin/
 echo "Successfully added executables to your local binaries"
 
 if [ -d $HOME/.shortcuts ];then
